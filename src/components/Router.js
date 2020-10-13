529a45231693789647cad8e57590ebdd296b8f73
DAWUNHAN <dawun.han@gmail.com>
2020-10-13 17:15:34 +1100

    Authentication and twitter function

---
+++ src/components/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation></Navigation>}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj}> </Home>
+            </Route>
+            <Route exact path="/profile">
+              <Profile></Profile>
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth></Auth>
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+export default AppRouter;
---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { dbService } from "fbase";
+import Dweet from "components/Dweet";
+
+const Home = ({ userObj }) => {
+  const [dweet, setDweet] = useState("");
+  const [dweets, setDweets] = useState([]);
+
+  useEffect(() => {
+    dbService.collection("dweets").onSnapshot((snapshot) => {
+      const dweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setDweets(dweetArray);
+    });
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("dweets").add({
+      text: dweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setDweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setDweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={dweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        ></input>
+        <input type="submit" value="Dwitter"></input>
+      </form>
+      <div>
+        {dweets.map((dweet) => (
+          <Dweet
+            key={dweet.id}
+            dweetObj={dweet}
+            isOwner={dweet.creatorId === userObj.uid}
+          ></Dweet>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Home;
