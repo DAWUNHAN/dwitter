25b07bb701a0b643fba9f05d93640f1d22b283ac
DAWUNHAN <dawun.han@gmail.com>
2020-10-15 16:29:15 +1100

    Updates

---
+++ precache-manifest.9dabb8efc482e8da094d8136c1f0fd02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3d83ffe9cb95b0e4511170e6859c33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c94b3b6067da59cea9",
+    "url": "/static/css/main.8ce51982.chunk.css"
+  },
+  {
+    "revision": "2cb8a77f40ff4972a579",
+    "url": "/static/js/2.792cf042.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/static/js/2.792cf042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c94b3b6067da59cea9",
+    "url": "/static/js/main.5129ceb7.chunk.js"
+  },
+  {
+    "revision": "920cb1c1cf8f8191a441",
+    "url": "/static/js/runtime-main.07e96b79.js"
+  }
+]);
